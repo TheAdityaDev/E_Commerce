481e3e242464bb11f52c383d9908ec661f68070c
TheAdityaDev <adityarvindapatil@gmail.com>
2026-05-25 13:39:16 +0530

    updated files env's

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,59 +4,24 @@ import tailwindcss from "@tailwindcss/vite";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
+  base: "/",
   server: {
     host: true,
     port: 5173,
   },
-
   plugins: [
     react(),
     tailwindcss(),
-
     VitePWA({
       registerType: "autoUpdate",
-
       includeAssets: ["favicon.svg", "robots.txt"],
-
       manifest: {
-        name: "My App",
-        short_name: "App",
-        description: "My Vite + React PWA",
+        name: "Ram Mart",
+        short_name: "Ram Mart",
         theme_color: "#ffffff",
-
         icons: [
-          {
-            src: "pwa-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
-      },
-
-      workbox: {
-        runtimeCaching: [
-          {
-            urlPattern: ({ request }) =>
-              ["document", "script", "style"].includes(
-                request.destination
-              ),
-
-            handler: "NetworkFirst",
-
-            options: {
-              cacheName: "assets-cache",
-
-              expiration: {
-                maxEntries: 100,
-                maxAgeSeconds: 60 * 60 * 24 * 7,
-              },
-            },
-          },
+          { src: "pwa-192x192.png", sizes: "192x192", type: "image/png" },
+          { src: "pwa-512x512.png", sizes: "512x512", type: "image/png" },
         ],
       },
     }),
